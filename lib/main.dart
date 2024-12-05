f68401cfe65272f9a0f230452b6f64979fe3c5cf
Pineda Mauricio Gaston <pinedamg@gmail.com>
2024-12-05 10:17:29 -0300

    feat: configured Firebase for Android and added initialization setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:walletar/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('WalletAR')),
-        body: Center(child: Text('Firebase Configurado')),
+        body: Center(child: Text('Firebase Configurado Correctamente')),
       ),
     );
   }
